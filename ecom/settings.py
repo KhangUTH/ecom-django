fdef7816f2eb83c1350873556b9d12e27737c645
khang <khangtm2739@ut.edu.vn>
2025-12-20 14:17:40 +0700

    change middleware

--- ecom/settings.py
+++ ecom/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenowhite.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ecom.urls'
